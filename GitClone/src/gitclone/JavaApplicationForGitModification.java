2c460ff3f9a302d8665a5926e63614dc64b17f56
Usuario <Usuario@Usuario-PC>
2018-11-13 19:15:01 -0600

    Great Commit

---
+++ GitClone/src/gitclone/JavaApplicationForGitModification.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitclone;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JavaApplicationForGitModification {
+    
+    
+    public String getDescription(){
+        
+        return "Tetx Documents (*.txt)";
+        
+    }
+    
+}
